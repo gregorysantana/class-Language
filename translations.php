1437b65616ab43de57b0f2d4f71e84839bf90224
Gregory Santana <gregorymhk@gmail.com>
2023-01-20 11:54:28 -0400

    Create translations.php

---
+++ translations.php
@@ -0,0 +1,11 @@
+<?php
+$translations = array(
+  "en_US" => array(
+    "Hello" => "Hello",
+    "Welcome" => "Welcome"
+  ),
+  "es_ES" => array(
+    "Hello" => "Hola",
+    "Welcome" => "Bienvenido"
+  )
+);
